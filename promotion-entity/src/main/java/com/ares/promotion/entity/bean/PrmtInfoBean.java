6c47b53f9d8feb694627980f034d0be8c1728774
wangshuai <1023784233@qq.com>
2017-04-26 23:35:29 +0800

    修改了这个文件

--- promotion-entity/src/main/java/com/ares/promotion/entity/bean/PrmtInfoBean.java
+++ promotion-entity/src/main/java/com/ares/promotion/entity/bean/PrmtInfoBean.java
@@ -1,5 +1,19 @@
 package com.ares.promotion.entity.bean;
 
 public class PrmtInfoBean {
-
+	private String prmtName;
+	private String prmtType;
+	public String getPrmtName() {
+		return prmtName;
+	}
+	public void setPrmtName(String prmtName) {
+		this.prmtName = prmtName;
+	}
+	public String getPrmtType() {
+		return prmtType;
+	}
+	public void setPrmtType(String prmtType) {
+		this.prmtType = prmtType;
+	}
+	
 }
